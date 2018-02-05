f5cfc4c5409f37caa0dd04e950481915cf0ad2c2
Colby Morrison <colbyacts@gmail.com>
2018-02-05 02:31:25 -0500

    parse for snippet

--- search.py
+++ search.py
@@ -11,16 +11,17 @@ def main():
                     developerKey="AIzaSyAeOp7L_cXV_VGjXuXkj_in8I1-pccZEEU")
 
     res = service.cse().list(q='lectures',
-                             cx='004635228232604600486:dehcqnd7kkq', num=10).execute()
-    dct = json.loads(json.dumps(res)).get('items').get('snippet')
-    print dct
+                             cx='004635228232604600486:dehcqnd7kkq', num=1).execute()
+    items = json.loads(json.dumps(res)).get('items').pop()
+    snippet = items.get('snippet')
+    print snippet
 
 
 
-
-def snippet(dct):
-    if "items" in dct:
-        return dct['items']
+#
+# def snippet(dct):
+#     if "items" in dct:
+#         return dct['items']
 
 
 if __name__ == '__main__':
