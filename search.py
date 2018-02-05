0e983ee6b9bd4f5d818ea9098712e2472ba4bc07
Colby Morrison <colbyacts@gmail.com>
2018-02-05 02:49:09 -0500

    finds str snippets

--- search.py
+++ search.py
@@ -5,23 +5,20 @@ import simplejson as json
 from googleapiclient.discovery import build
 
 
-
-def main():
+def snippets(str):
     service = build("customsearch", "v1",
                     developerKey="AIzaSyAeOp7L_cXV_VGjXuXkj_in8I1-pccZEEU")
 
-    res = service.cse().list(q='lectures',
-                             cx='004635228232604600486:dehcqnd7kkq', num=1).execute()
-    items = json.loads(json.dumps(res)).get('items').pop()
-    snippet = items.get('snippet')
-    print snippet
-
-
-
-#
-# def snippet(dct):
-#     if "items" in dct:
-#         return dct['items']
+    res = service.cse().list(q=str,
+                             cx='004635228232604600486:dehcqnd7kkq', num=9).execute()
+    items = json.loads(json.dumps(res)).get('items')
+    snippet = []
+    count = 0
+    for i in items:
+        j = items.pop(count)
+        snippet.append(j.get('snippet'))
+        count = count + 1
+    return snippet
 
 
 if __name__ == '__main__':
