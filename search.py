6c5e255daeb4d46dd4d61dfd726308995f25161f
Colby Morrison <colbyacts@gmail.com>
2018-04-06 17:30:19 -0400

    searching functions

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from googleapiclient.discovery import build
 import pytesseract
+import os
 
 
 def img_to_text(file):
@@ -8,7 +9,11 @@ def img_to_text(file):
     ques, ans0, ans1, ans2 = "", "", "", ""
     ln = 0
 
-    for i in tess.split('\n'):
+    tess = os.linesep.join([s for s in tess.splitlines() if s])
+    tess = tess.replace('"', ' ')
+    tess = tess.replace('\'', ' ')
+
+    for i in tess.splitlines():
         if not i.strip(): continue
         if not i[0:1].isalpha():
             ln = ln + 1
@@ -16,9 +21,9 @@ def img_to_text(file):
         ques += i
         ln = ln + 1
         if i.endswith("?"):
-            ans0 = tess.splitlines()[ln + 2]
-            ans1 = tess.splitlines()[ln + 4]
-            ans2 = tess.splitlines()[ln + 6]
+            ans0 = tess.splitlines()[ln]
+            ans1 = tess.splitlines()[ln+1]
+            ans2 = tess.splitlines()[ln+2]
             break
 
     ques = ques.replace("\"","")
@@ -27,29 +32,46 @@ def img_to_text(file):
 
 
 def google_search(question, answer):
-    search_str = "{} \"{}\"".format(question, answer)
+    search_str = "{} {}".format(question, answer)
     service = build("customsearch", "v1",
                     developerKey="AIzaSyCVsjb-Ar3mE-oZRiTYjsG4qLm85NxLkws")
-    res = service.cse().list(q=search_str, cx="004635228232604600486:dehcqnd7kkq", num=1).execute()
-    res = res['searchInformation']
-    return res['totalResults']
+    print(search_str)
+    res = service.cse().list(q="What did Yankee Doodle stickin his cap? Feather", cx="004635228232604600486:dehcqnd7kkq", num=1).execute()
+
+
+    search = res['searchInformation']
+    spell = res['spelling']
+    print(spell['correctedQuery'])
+    print(search['totalResults'])
+    return {'ans': answer, 'res':  search['totalResults']}
 
 
 def search_answer(q_as, num):
     answer = q_as['ans{}'.format(num)]
-    results = google_search(q_as['ques'], answer)
-    print("{}: {}\n".format(answer, results))
+    return google_search(q_as['ques'], answer)
 
 
 def search(file):
     q_as = img_to_text(file)
+    results = []
     for i in range(0, 3):
-        search_answer(q_as, i)
-
+        results.append(search_answer(q_as, i))
+    
+    best = max(results[0]['res'], results[1]['res'], results[2]['res'])
+    
+    for dct in results:
+        if dct['res'] == best:
+            print("{} with {} results\n\n".format(dct['ans'], dct['res']))
+            results.remove(dct)
+        
+    for dct in results:
+        print("{} with {} results".format(dct['ans'], dct['res']))
+        
 
 def main():
-    search("resources/screenshot-1.png")
-
+    #search("resources/screenshot-3.png")
+    google_search("hi", "hi")
+    #print(img_to_text("resources/screenshot-8.png"))
 
 if __name__ == "__main__":
     main()
