94162af64acedb74b391772b684e4595bcb376ec
Colby Morrison <colbyacts@gmail.com>
2018-04-05 23:22:40 -0400

    Working better

--- search.py
+++ search.py
@@ -9,24 +9,28 @@ def img_to_text(file):
     ln = 0
 
     for i in tess.split('\n'):
-        if i.strip():
-            ques += i
+        if not i.strip(): continue
+        if not i[0:1].isalpha():
             ln = ln + 1
-            if i.endswith("?"):
-                ans0 = tess.splitlines()[ln + 2]
-                ans1 = tess.splitlines()[ln + 4]
-                ans2 = tess.splitlines()[ln + 6]
-                break
+            continue
+        ques += i
+        ln = ln + 1
+        if i.endswith("?"):
+            ans0 = tess.splitlines()[ln + 2]
+            ans1 = tess.splitlines()[ln + 4]
+            ans2 = tess.splitlines()[ln + 6]
+            break
 
+    ques = ques.replace("\"","")
     q_as = {'ques': ques, 'ans0': ans0, 'ans1': ans1, 'ans2': ans2}
     return q_as
 
 
 def google_search(question, answer):
-    search_str = "{}\"{}\"".format(question, answer)
+    search_str = "{} \"{}\"".format(question, answer)
     service = build("customsearch", "v1",
                     developerKey="AIzaSyCVsjb-Ar3mE-oZRiTYjsG4qLm85NxLkws")
-    res = service.cse().list(q=search_str, cx="004635228232604600486:dehcqnd7kkq", num=10).execute()
+    res = service.cse().list(q=search_str, cx="004635228232604600486:dehcqnd7kkq", num=1).execute()
     res = res['searchInformation']
     return res['totalResults']
 
@@ -39,12 +43,12 @@ def search_answer(q_as, num):
 
 def search(file):
     q_as = img_to_text(file)
-    for i in range(0, 2):
+    for i in range(0, 3):
         search_answer(q_as, i)
 
 
 def main():
-    search("results/screenshot-5.png")
+    search("resources/screenshot-1.png")
 
 
 if __name__ == "__main__":
